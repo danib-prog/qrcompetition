be3d6aafdb19a561772c150a5659ac0b33c956ee
danib-prog <9buza.dani@gmail.com>
2022-12-22 01:17:05 +0100

    security issue solved

--- emph/emph/settings.py
+++ emph/emph/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%pr3-fm)l3)nrjdv48vyx$@dt%#fv68n3&y*m(+ecm6i1dopi4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
